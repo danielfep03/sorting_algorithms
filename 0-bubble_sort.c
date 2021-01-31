e87a8abb54c9cbf4f09c1ca34b46ae0a3ff4dbc0
Nicolas <nico15935746@gmail.com>
2021-01-31 02:45:44 -0500

    THE FASTEST WAY

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,31 +8,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, a = 0, temp;
+	size_t i = 0, j = 0, temp = size;
 	int tmp;
 
-	temp = size;
 	if (size <= 1 || array == NULL)
 		return;
 	for (; j < size; j++)
 	{
-		for (; i < temp; i++)
+		for (i = 0; i < temp - 1; i++)
 		{
-			if (!array[i + 1])
-				break;
 			if (array[i] >= array[i + 1])
 			{
 				tmp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = tmp;
 				print_array(array, size);
-				a = 0;
 			}
 			if (i == temp - 1)
-			{
 				temp -= 1;
-			}
 		}
-		i = 0;
 	}
 }
