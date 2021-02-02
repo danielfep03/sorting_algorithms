80f17dd24d8925ba307fd328589757021bbe26b3
Nicolas <nico15935746@gmail.com>
2021-02-01 23:42:16 -0500

    The worst algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,13 +15,14 @@ void selection_sort(int *array, size_t size)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
-		{
 			if (array[min] > array[j])
 				min = j;
-		}
+		if (min != i)
+		{
 		tmp = array[i];
 		array[i] = array[min];
 		array[min] = tmp;
 		print_array(array, size);
+		}
 	}
 }
