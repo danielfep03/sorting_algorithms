b817f45085cb20807f5c51abe08c97550611e846
Nicolas <nico15935746@gmail.com>
2021-02-01 23:44:33 -0500

    Betty

--- sort.h
+++ sort.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <strings.h>
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -21,7 +22,6 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-void change_node_position(listint_t **head, unsigned int idx, unsigned int idx2);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 
