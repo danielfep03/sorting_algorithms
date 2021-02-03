b3743613b20b0909f76583779fd886c2e32833bd
Nicolas <nico15935746@gmail.com>
2021-02-02 19:39:47 -0500

    Gross hack because Igor doesn't know how to code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,8 +5,9 @@ void swap(listint_t **tmp)
 	listint_t *aux;
 
 	aux = (*tmp)->prev->prev;
+	printf("\n\n AUXILIAR: %d\n\n", aux->n);
 	if ((*tmp)->prev->prev)
-		(*tmp)->prev->prev->next =  *tmp;
+		(*tmp)->prev->prev->next = *tmp;
 	if ((*tmp)->next)
 		(*tmp)->next->prev = (*tmp)->prev;
 	(*tmp)->prev->next = (*tmp)->next;
@@ -14,6 +15,7 @@ void swap(listint_t **tmp)
 	(*tmp)->next = (*tmp)->prev;
 	(*tmp)->prev = aux;
 }
+
 /**
  *
  *
@@ -36,12 +38,14 @@ void insertion_sort_list(listint_t **list)
 			swap(&tmp);
 			print_list(*list);
 			tmp2 = tmp;
+			printf("==============\nTMP2 = %d, PREV %d, NEXT %d\n==============\n", tmp2->n, tmp2->prev->n, tmp2->next->n);
 			while(tmp2->n < tmp2->prev->n)
 			{
 				swap(&tmp2);
 				tmp2 = tmp->prev;
 				print_list(*list);
 				printf("\n%d\n", tmp2->n);
+				printf("====WHILE====\nTMP2 = %d, PREV %d, NEXT %d\n==============\n", tmp2->n, tmp2->prev->n, tmp2->next->n);
 			}
 		}
 		tmp = tmp->next;
