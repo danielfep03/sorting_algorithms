56f238beb4fee7053259403a8f7a4bcbe87db196
Nicolas <nico15935746@gmail.com>
2021-02-03 22:21:43 -0500

    New header

--- sort.h
+++ sort.h
@@ -25,5 +25,9 @@ void swap(listint_t **tmp, listint_t **list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quicksort(int *array, ssize_t start, ssize_t next, size_t size);
+void swapp(int *n1, int *n2);
+int partition(int *array, ssize_t start, ssize_t next, size_t size);
 
 #endif
